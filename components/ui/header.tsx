05873e5eb8e0414372c48a3d32730c01cc8949c3
rayjay8 <rayjay9@proton.me>
2025-06-18 22:23:57 +0530

    header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="w-full fixed top-0 left-0 right-0 z-50 flex items-center justify-between py-4 px-8 md:px-50 bg-black text-white">
+    <header className="w-full fixed top-0 left-0 right-0 z-50 flex items-center justify-between py-4 px-8 md:px-50 bg-[#151517]/20 backdrop-blur-md text-white border-b border-[#252525]">
       {/* Left: Logo + Nav */}
       <div className="flex items-center gap-10">
         <div className="font-bold text-xl tracking-tight text-[#CBFB44] select-none">
@@ -37,7 +37,7 @@ export default function Header() {
       </div>
       {/* Right: Sign In Button */}
       <div>
-        <button className="px-4 py-1 rounded-full bg-[#CBFB44] text-black font-semibold text-md transition-colors hover:bg-lime-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#CBFB44]">
+        <button className="px-6 py-1 rounded-full bg-[#CBFB44] text-black font-medium text-md transition-colors hover:bg-lime-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#CBFB44]">
           Sign In
         </button>
       </div>
